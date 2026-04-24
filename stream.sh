9bdc626812f60af74fdd45af595532e96bf31c7b
Martin Pfister <martin-pfister@users.noreply.github.com>
2026-04-24 13:45:50 +0200

    Reliability: add aresample sync and global_header for long-term A/V stability

--- stream.sh
+++ stream.sh
@@ -14,11 +14,11 @@ while true; do
 
     ffmpeg \
         -i "$SRT_INPUT_URL" \
-        -filter_complex "[0:a]pan=stereo|c0=c0|c1=c0[a_left];[0:a]pan=stereo|c0=c1|c1=c1[a_right]" \
+        -filter_complex "[0:a]pan=stereo|c0=c0|c1=c0,aresample=async=1[a_left];[0:a]pan=stereo|c0=c1|c1=c1,aresample=async=1[a_right]" \
         -map 0:v -map "[a_left]" -c:v copy -c:a aac -b:a 160k \
-            -f flv "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_KEY_LEFT}" \
+            -flags +global_header -f flv "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_KEY_LEFT}" \
         -map 0:v -map "[a_right]" -c:v copy -c:a aac -b:a 160k \
-            -f flv "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_KEY_RIGHT}" &
+            -flags +global_header -f flv "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_KEY_RIGHT}" &
     
     FFMPEG_PID=$!
     wait $FFMPEG_PID
