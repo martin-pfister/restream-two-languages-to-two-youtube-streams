120ca5dfdfa523ca30be0b98ac4587932ac9a9a0
Martin Pfister <martin-pfister@users.noreply.github.com>
2026-04-24 13:06:19 +0200

    Add passphrase

--- stream.sh
+++ stream.sh
@@ -5,8 +5,13 @@ while true; do
     echo "============================================="
     echo "Waiting for incoming SRT connection from OBS..."
     echo "============================================="
+    SRT_INPUT_URL="srt://0.0.0.0:9000?mode=listener"
+    if [ -n "$SRT_INPUT_PASSPHRASE" ]; then
+        SRT_INPUT_URL="${SRT_INPUT_URL}&passphrase=${SRT_INPUT_PASSPHRASE}"
+    fi
+
     ffmpeg \
-        -listen 1 -i "srt://0.0.0.0:9000?mode=listener" \
+        -listen 1 -i "$SRT_INPUT_URL" \
         -filter_complex "[0:a]pan=stereo|c0=c0|c1=c0[a_en];[0:a]pan=stereo|c0=c1|c1=c1[a_de]" \
         -map 0:v -map "[a_en]" -c:v copy -c:a aac -b:a 160k \
             -f flv "rtmp://a.rtmp.youtube.com/live2/${YT_KEY_EN}" \
